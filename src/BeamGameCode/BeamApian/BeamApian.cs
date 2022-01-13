5720974b121168733e3fad755c62a164d7d5d812
jim berry <jim@bigfattail.com>
2022-01-13 08:05:36 -0500

    Separate BeamApianFactory into its own module

--- src/BeamGameCode/BeamApian/BeamApian.cs
+++ src/BeamGameCode/BeamApian/BeamApian.cs
@@ -14,38 +14,6 @@ namespace BeamGameCode
         public BeamApianPeer(string _p2pId, string _appHelloData) : base(_p2pId, _appHelloData) { }
     }
 
-    public static class BeamApianFactory
-    {
-        public static readonly List<string> ApianGroupTypes = new List<string>()
-        {
-            SinglePeerGroupManager.kGroupType,
-            CreatorSezGroupManager.kGroupType,
-            LeaderSezGroupManager.kGroupType
-        };
-
-        public static BeamApian Create(string apianGroupType, IBeamGameNet beamGameNet, BeamAppCore appCore)
-        {
-            BeamApian result;
-            switch (apianGroupType)
-            {
-            case SinglePeerGroupManager.kGroupType:
-                result = new BeamApianSinglePeer(beamGameNet, appCore);
-                break;
-            case CreatorSezGroupManager.kGroupType:
-                result =  new BeamApianCreatorSez(beamGameNet, appCore);
-                break;
-            case LeaderSezGroupManager.kGroupType:
-                result =  new BeamApianLeaderSez(beamGameNet, appCore);
-                break;
-            default:
-                UniLogger.GetLogger("Apian").Warn($"BeamApianFactory.Create() Unknown GroupType: {apianGroupType}");
-                result = null;
-                break;
-            }
-            return result;
-        }
-    }
-
     public abstract class BeamApian : ApianBase
     {
 
---
+++ src/BeamGameCode/BeamApian/BeamApianFactory.cs
@@ -0,0 +1,44 @@
+using System.Net.WebSockets;
+using System;
+using System.Collections.Generic;
+using Apian;
+using Newtonsoft.Json;
+using UnityEngine;
+using UniLog;
+using static UniLog.UniLogger; // for SID()
+
+namespace BeamGameCode
+{
+    public static class BeamApianFactory
+    {
+        public static readonly List<string> ApianGroupTypes = new List<string>()
+        {
+            SinglePeerGroupManager.kGroupType,
+            CreatorSezGroupManager.kGroupType,
+            //LeaderSezGroupManager.kGroupType
+        };
+
+        public static BeamApian Create(string apianGroupType, IBeamGameNet beamGameNet, BeamAppCore appCore)
+        {
+            BeamApian result;
+            switch (apianGroupType)
+            {
+            case SinglePeerGroupManager.kGroupType:
+                result = new BeamApianSinglePeer(beamGameNet, appCore);
+                break;
+            case CreatorSezGroupManager.kGroupType:
+                result =  new BeamApianCreatorSez(beamGameNet, appCore);
+                break;
+            //case LeaderSezGroupManager.kGroupType:
+            //    result =  new BeamApianLeaderSez(beamGameNet, appCore);
+            //    break;
+            default:
+                UniLogger.GetLogger("Apian").Warn($"BeamApianFactory.Create() Unknown GroupType: {apianGroupType}");
+                result = null;
+                break;
+            }
+            return result;
+        }
+    }
+
+}
